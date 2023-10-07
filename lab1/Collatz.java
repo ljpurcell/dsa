71789bca1300d152cf264eda38c12b871c5b58d5
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-10-07 11:28:37 +1100

    Tests for emptySpaceExists and maxTileExists passing.

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,5 +1,5 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ * @author Lyndon
  */
 public class Collatz {
 
@@ -10,7 +10,7 @@ public class Collatz {
 
     public static void main(String[] args) {
         int n = 5;
-        System.out.print(n + " ");
+            System.out.print(n + " ");
         while (n != 1) {
             n = nextNumber(n);
             System.out.print(n + " ");
--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -5,7 +5,7 @@ import java.util.Observable;
 
 
 /** The state of a game of 2048.
- *  @author TODO: YOUR NAME HERE
+ *  @author ljpurcell
  */
 public class Model extends Observable {
     /** Current contents of the board. */
@@ -137,7 +137,14 @@ public class Model extends Observable {
      *  Empty spaces are stored as null.
      * */
     public static boolean emptySpaceExists(Board b) {
-        // TODO: Fill in this function.
+       for (int col = 0; col < b.size(); col++) {
+           for (int row = 0; row < b.size(); row++) {
+              if (b.tile(col, row) == null) {
+                  return true;
+              }
+           }
+       }
+
         return false;
     }
 
@@ -147,7 +154,14 @@ public class Model extends Observable {
      * given a Tile object t, we get its value with t.value().
      */
     public static boolean maxTileExists(Board b) {
-        // TODO: Fill in this function.
+        for (int col = 0; col < b.size(); col++) {
+            for (int row = 0; row < b.size(); row++) {
+                Tile t = b.tile(col, row);
+                if (t != null && t.value() == MAX_PIECE) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -158,7 +172,12 @@ public class Model extends Observable {
      * 2. There are two adjacent tiles with the same value.
      */
     public static boolean atLeastOneMoveExists(Board b) {
-        // TODO: Fill in this function.
+        if (emptySpaceExists(b)) {
+            return true;
+        }
+        else {
+
+        }
         return false;
     }
 
