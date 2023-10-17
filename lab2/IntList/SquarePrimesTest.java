4e6dbf0edea8c71a430332f7210ba213d546d032
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-10-18 10:37:17 +1100

    Created failing test. Still need to find source of failure

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -25,4 +25,60 @@ public class SquarePrimesTest {
         assertEquals("49", lst.toString());
         assertTrue(changed);
     }
+
+      @Test
+    public void testSquarePrimesSingleCompositeElement() {
+        IntList lst = IntList.of(8);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("8", lst.toString());
+        assertFalse(changed);
+    }
+
+    @Test
+    public void testSquarePrimesFirstElementIsPrime() {
+        IntList lst = IntList.of(13, 8, 4);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("169 -> 8 -> 4", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesLastElementIsPrime() {
+        IntList lst = IntList.of(8, 9, 13);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("8 -> 9 -> 169", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesFirstElementIsComposite() {
+        IntList lst = IntList.of(9, 8, 4);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("9 -> 8 -> 4", lst.toString());
+        assertFalse(changed);
+    }
+
+    @Test
+    public void testSquarePrimesLastElementIsComposite() {
+        IntList lst = IntList.of(8, 8, 12);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("8 -> 8 -> 12", lst.toString());
+        assertFalse(changed);
+    }
+
+    @Test
+    public void testSquarePrimesTestWithPrimeOverTwenty() {
+        IntList lst = IntList.of(8, 8, 23, 12);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("8 -> 8 -> 529 -> 12", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test // TODO run debugger over this test
+    public void testSquarePrimesTestWithLongList() {
+        IntList lst = IntList.of(8, 8, 23, 12, 13, 10, 7, 31, 41);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("8 -> 8 -> 529 -> 12 -> 169 -> 10 -> 49 -> 961 -> 1681", lst.toString());
+        assertTrue(changed);
+    }
 }
