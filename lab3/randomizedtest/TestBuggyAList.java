207ab0aa39d29e41dc217be02c8aee7efaccfc8f
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-10-22 09:26:00 +0900

    Randomized tests for non-resive AList - Lab 3

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -2,11 +2,58 @@ package randomizedtest;
 
 import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> buggy = new BuggyAList<>();
+
+        for (int i = 0; i < 3; i++) {
+            correct.addLast(i);
+            buggy.addLast(i);
+        }
+
+        assertEquals(correct.size(), buggy.size());
+
+        assertEquals(correct.removeLast(), buggy.removeLast());
+        assertEquals(correct.removeLast(), buggy.removeLast());
+        assertEquals(correct.removeLast(), buggy.removeLast());
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+
+        int N = 500;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int size = L.size();
+                System.out.println("size: " + size);
+            } else if (L.size() > 0) {
+                if (operationNumber == 2) {
+                    // getLast
+                    int last = L.getLast();
+                    System.out.println("last: " + last);
+                } else if (operationNumber == 3) {
+                    // removeLast
+                    int last = L.removeLast();
+                    System.out.println("removeLast: " + last);
+                }
+            }
+        }
+    }
 }
