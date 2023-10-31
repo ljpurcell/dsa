1830e748dc3308cfe38107648f4b9d56fb353158
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-10-31 18:51:22 +0900

    Re-implement buggy Collatz from Lab1 - to fix in Lab 4

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,22 @@
 /** Class that prints the Collatz sequence starting from a given number.
- * @author Lyndon
+ *  @author YOUR NAME HERE
  */
 public class Collatz {
 
-    /** Returns the next number in a Collatz sequence ! */
+    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        return n % 2 == 0 ? n / 2 : 3 * n + 1;
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
+        } else {
+            return n * 2;
+        }
     }
 
     public static void main(String[] args) {
         int n = 5;
-            System.out.print(n + " ");
+        System.out.print(n + " ");
         while (n != 1) {
             n = nextNumber(n);
             System.out.print(n + " ");
