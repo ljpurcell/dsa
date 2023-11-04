95b2e5eb6eabe79c36aa928e6ef9e455c93b5cbe
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-11-04 11:06:35 +0900

    Outline for Commit - working on initial commit spec

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -1,26 +1,42 @@
 package gitlet;
 
-// TODO: any imports you need here
-
-import java.util.Date; // TODO: You'll likely use this in this class
+import java.util.Date;
 
 /** Represents a gitlet commit object.
  *  TODO: It's a good idea to give a description here of what else this Class
  *  does at a high level.
  *
- *  @author TODO
+ *  @author ljpurcell
  */
 public class Commit {
     /**
-     * TODO: add instance variables here.
-     *
      * List all instance variables of the Commit class here with a useful
      * comment above them describing what that variable represents and how that
-     * variable is used. We've provided one example for `message`.
+     * variable is used.
      */
+
+    private String id; // Hex-string from commit hash?
 
     /** The message of this Commit. */
     private String message;
 
-    /* TODO: fill in the rest of this class. */
+    /** The creator of the Commit. */
+    private String author;
+
+    /** The date the Commit was created. */
+    private Date date = new Date();
+
+    /** The time the Commit was created. */
+    private long unixTime = new Date().getTime();
+
+    /** A hex-string reference to the repo state */
+    private String parentCommitRef;
+
+    /** A hex-string reference to the repo state */
+    private String repoStateRef;
+
+    public Commit(String msg, String auth) {
+        message = msg;
+        author = auth;
+    }
 }
--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,14 +1,16 @@
 package gitlet;
 
 import java.io.File;
+
 import static gitlet.Utils.*;
 
 
-/** Represents a gitlet repository.
+/**
+ * Represents a gitlet repository.
  *  TODO: It's a good idea to give a description here of what else this Class
  *  does at a high level.
  *
- *  @author ljpurcell
+ * @author ljpurcell
  */
 public class Repository {
     /**
@@ -19,19 +21,26 @@ public class Repository {
      * variable is used. We've provided two examples for you.
      */
 
-    /** The current working directory. */
+    /**
+     * The current working directory.
+     */
     public static final File CWD = new File(System.getProperty("user.dir"));
-    /** The .gitlet directory. */
+    /**
+     * The .gitlet directory.
+     */
     public static final File GITLET_DIR = join(CWD, ".gitlet");
 
     public static void initialiseGitletRepo() {
         if (GITLET_DIR.exists()) {
             System.out.println("A Gitlet version-control system already exists in the current directory.");
+        } else if (!GITLET_DIR.mkdir()) {
+            throw new GitletException("Could not initialise Gitlet repository");
+        } else {
+           createInitialCommit();
         }
-        else {
-            if (!GITLET_DIR.mkdir()) {
-                throw new GitletException("Could not initialise Gitlet repository");
-            }
-        }
+    }
+
+    private void createInitialCommit() {
+        Commit c = new Commit();
     }
 }
