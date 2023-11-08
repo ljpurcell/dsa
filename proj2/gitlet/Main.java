a38e13865bb8546bebafc9c1b9b18e18f0dc3f45
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-11-08 10:28:30 +0900

    Add guard into Main for executing commands when not in repo

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,24 +1,35 @@
 package gitlet;
 
-/** Driver class for Gitlet, a subset of the Git version-control system.
- *  @author ljpurcell
+import java.util.Arrays;
+
+/**
+ * Driver class for Gitlet, a subset of the Git version-control system.
+ *
+ * @author ljpurcell
  */
 public class Main {
 
-    /** Usage: java gitlet.Main ARGS, where ARGS contains
-     *  <COMMAND> <OPERAND1> <OPERAND2> ... 
+    /**
+     * Usage: java gitlet.Main ARGS, where ARGS contains
+     * <COMMAND> <OPERAND1> <OPERAND2> ...
      */
     public static void main(String[] args) {
         // TODO: if args is empty, show help
         String firstArg = args[0];
-        switch(firstArg) {
-            case "init":
-                Repository.initialiseGitletRepo();
-                break;
-            case "add":
-                Repository.addFilesToStagingArea(args);
-                break;
-            // TODO: FILL THE REST IN
+        if (!Repository.GITLET_DIR.exists() && !firstArg.equals("init")) {
+            System.out.println("Not in an initialized Gitlet directory");
+        } else {
+
+            switch (firstArg) {
+                case "init":
+                    Repository.initialiseGitletRepo();
+                    break;
+                case "add":
+                    String[] files = Arrays.copyOfRange(args, 1, args.length);
+                    Repository.addFilesToStagingArea(files);
+                    break;
+                // TODO: FILL THE REST IN
+            }
         }
     }
 }
